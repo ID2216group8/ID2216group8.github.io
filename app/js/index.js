a1c7e6263b9841daf7d6d8b7c83d4aafb9e61c2a
Tim <sonnenpelzxx@gmail.com>
2023-02-07 20:32:29 +0100

    added mannequin and navigation

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+
+const plus = document.querySelector('#plus')
+const mannequin = document.querySelector('#mannequin')
+const settings = document.querySelector('#settings-dots')
+
+plus.addEventListener("click", function(){document.location.href = "change-cities.html";})
+mannequin.addEventListener("click", function(){document.location.href = "change-clothing.html";})
+settings.addEventListener("click", function(){document.location.href = "settings.html";})
+
---
+++ app/js/settings.js
@@ -0,0 +1,2 @@
+const close = document.querySelector('#close')
+close.addEventListener("click", function(){document.location.href = "index.html";})
