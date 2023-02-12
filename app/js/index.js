e317c10df84e8f5e36a23bc8b08cefd832699550
yukunzou <792445363@qq.com>
2023-02-12 23:21:56 +0100

    revise swipe to be compatible with phone screen

--- app/js/index.js
+++ app/js/index.js
@@ -17,5 +17,75 @@ clothingAlgorithm();
 var sliderTopMargin = (window.innerHeight - 8 - 43 - 381 - (0.2 * window.innerWidth)-106)/2;
 slider.style.marginTop = sliderTopMargin.toString() + "px";
 
+var startx, starty;
+//get angle
+function getAngle(angx, angy) {
+    return Math.atan2(angy, angx) * 180 / Math.PI;
+};
+
+// 1up 2down 3left 4right 0click
+function getDirection(startx, starty, endx, endy) {
+    var angx = endx - startx;
+    var angy = endy - starty;
+    var result = 0;
+
+    //if swipe distance too short
+    if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+        return result;
+    }
+
+    var angle = getAngle(angx, angy);
+    if (angle >= -135 && angle <= -45) {
+        result = 1;
+    } else if (angle > 45 && angle < 135) {
+        result = 2;
+    } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+        result = 3;
+    } else if (angle >= -45 && angle <= 45) {
+        result = 4;
+    }
+    return result;
+}
+
+//finger touch screen
+document.addEventListener("touchstart", function(e){
+    startx = e.touches[0].pageX;
+    starty = e.touches[0].pageY;
+}, false);
+
+//finger leave screen
+document.addEventListener("touchend", async function(e) {
+    var endx, endy;
+    endx = e.changedTouches[0].pageX;
+    endy = e.changedTouches[0].pageY;
+    var direction = getDirection(startx, starty, endx, endy);
+    switch (direction) {
+        case 0:
+            break;
+        case 1:
+            let data = await getCurrentWeather(getCurrentCity())
+            console.log(data)
+            const weather = data[1]
+            const temperature = data[0]
+            layer.open({
+                type: 1,
+                title: 'Weather Information',
+                skin: 'layui-layer-lan',
+                content: '<div style="padding: 20px;">' +
+                    '<p><font color="#FF0000" size="6">' + weather + '</font></p>' +
+                    '<p><font color="#0000FF" size="6">' + temperature + '°C</font></p>' +
+                    '</div>'
+            });
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
+}, false);
 
 
