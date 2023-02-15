c3c049862757f69773c10f8fda34bda402a16290
Yukun Zou <yukunzou@kth.se>
2023-02-15 13:57:22 +0100

    1

--- app/js/change-clothing.js
+++
@@ -1,8 +0,0 @@
-
-
-
-const close = document.querySelector('#close')
-
-close.addEventListener("click", function(){document.location.href = "index.html";})
-    const selecthat = document.querySelector('#selecthat')
-    
